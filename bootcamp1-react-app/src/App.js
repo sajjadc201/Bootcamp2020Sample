d695d7be19412beaa7651e511728c41bcfb76daf
sajjadc201 <67755341+sajjadc201@users.noreply.github.com>
2020-07-03 16:12:32 +0500

    added in class 2

---
+++ bootcamp1-react-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Allah is One
+    </div>
+  );
+}
+
+export default App;
